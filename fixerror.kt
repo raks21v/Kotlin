8dc3c92609c4e8d35aec91688cb09183dfa40c0b
Rakshitha V <127879661+raks21v@users.noreply.github.com>
2026-02-23 13:39:22 +0530

    Fix compile errors in chat message program
    Fixed compile errors in the Kotlin program by correcting string delimiters and ensuring proper syntax.

---
+++ fixerror.kt
@@ -0,0 +1,19 @@
+/*
+ This program prints a message that notifies the user that they received a chat message from a friend.
+
+
+fun main() { 
+    println("New chat message from a friend'}
+}
+Can you figure out the root cause of the compile errors in this program and fix them?
+Does the code use appropriate symbols to indicate the open and close of the string and function argument?
+Hint: You can use Kotlin Playground to run the code and view the compilation errors.
+
+After you fix the errors, the program should compile without errors and print this output:
+
+
+New chat message from a friend
+ */
+fun main() {
+    println("New chat message from a friend")
+}
