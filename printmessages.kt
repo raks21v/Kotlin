b4c8810f9bfbfe334f5f96e4f726ab7a89c4de77
Rakshitha V <127879661+raks21v@users.noreply.github.com>
2026-02-23 13:36:38 +0530

    Implement main function to print messages
    Add main function to print multiple messages

---
+++ printmessages.kt
@@ -0,0 +1,11 @@
+/*
+ Can you write a main() function that prints these messages on four separate lines?
+
+Use the val keyword when the value doesn't change. 
+Use the var keyword when the value can change.
+When you define a function, you define the parameters that can be passed to it. 
+When you call a function, you pass arguments for the parameters.
+ */
+fun main() {
+    println("Use the val keyword when the value doesn't change. \nUse the var keyword when the value can change.\nWhen you define a function, you define the parameters that can be passed to it.\nWhen you call a function, you pass arguments for the parameters.")
+}
